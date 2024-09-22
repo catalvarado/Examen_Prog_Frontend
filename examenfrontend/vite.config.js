b3b9f8b1aad54009a5b103615abbf9aeb5bf6daa
catalvarado <catalvaradom@gmail.com>
2024-09-21 22:49:09 -0300

    Despliegue de la aplicación Vue.js 3 en GitHub Pages

--- examenfrontend/vite.config.js
+++ examenfrontend/vite.config.js
@@ -3,10 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+        base: process.env.NODE_ENV === 'production'
+        ?'/Examen_Prog_Frontend/'
+        :'/',
   plugins: [
-    vue(),
+    vue()
   ],
   resolve: {
     alias: {
